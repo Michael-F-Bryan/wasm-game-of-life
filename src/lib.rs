558d03d53350b8440878366996d728013bf93ec6
Michael Bryan <michaelfbryan@gmail.com>
2018-11-21 22:02:39 +0800

    Hooked up rendering and made a "Player" wrapper

--- src/lib.rs
+++ src/lib.rs
@@ -4,6 +4,7 @@ extern crate wasm_bindgen;
 mod utils;
 
 use cfg_if::cfg_if;
+use std::fmt::{self, Display, Formatter};
 use wasm_bindgen::prelude::*;
 
 cfg_if! {
@@ -29,6 +30,7 @@ pub enum Cell {
     Alive = 1,
 }
 
+#[wasm_bindgen]
 #[derive(Clone, PartialEq, Eq)]
 pub struct Universe {
     width: usize,
@@ -37,15 +39,10 @@ pub struct Universe {
 }
 
 impl Universe {
-    pub fn new(width: usize, height: usize) -> Universe {
-        Universe {
-            width,
-            height,
-            cells: vec![Cell::Dead; width * height],
-        }
-    }
-
     pub fn live_neighbour_count(&self, x: usize, y: usize) -> usize {
+        debug_assert!(x < self.width);
+        debug_assert!(y < self.height);
+
         let mut count = 0;
         self.foreach_neighbour(x, y, |x, y| {
             let cell = self.cells[self.index_of(x, y)];
@@ -57,7 +54,7 @@ impl Universe {
         count
     }
 
-    pub fn foreach_neighbour<F>(
+    fn foreach_neighbour<F>(
         &self,
         centre_x: usize,
         centre_y: usize,
@@ -85,13 +82,21 @@ impl Universe {
         }
     }
 
-    fn index_of(&self, row: usize, column: usize) -> usize {
-        row * self.width + column
+    fn index_of(&self, x: usize, y: usize) -> usize {
+        y * self.width + x
     }
 }
 
 #[wasm_bindgen]
 impl Universe {
+    pub fn new(width: usize, height: usize) -> Universe {
+        Universe {
+            width,
+            height,
+            cells: vec![Cell::Dead; width * height],
+        }
+    }
+
     pub fn tick(&self) -> Universe {
         let mut next = self.clone();
 
@@ -106,6 +111,32 @@ impl Universe {
         }
 
         next
+    }
+
+    pub fn render(&self) -> String {
+        self.to_string()
+    }
+
+    pub fn set_cell(&mut self, x: usize, y: usize, alive: bool) {
+        let ix = self.index_of(x, y);
+
+        if let Some(cell) = self.cells.get_mut(ix) {
+            *cell = if alive { Cell::Alive } else { Cell::Dead };
+        }
+    }
+}
+
+impl Display for Universe {
+    fn fmt(&self, f: &mut Formatter) -> fmt::Result {
+        for line in self.cells.as_slice().chunks(self.width as usize) {
+            for &cell in line {
+                let symbol = if cell == Cell::Dead { '◻' } else { '◼' };
+                write!(f, "{}", symbol)?;
+            }
+            write!(f, "\n")?;
+        }
+
+        Ok(())
     }
 }
 
--- www/index.js
+++ www/index.js
@@ -1,3 +1,72 @@
-import * as wasm from "game-of-life";
+import { Universe } from "game-of-life";
 
-wasm.greet("someone");
+console.log(Universe);
+
+const pre = document.getElementById("game-of-life-canvas");
+
+const PLAYING = Symbol();
+const PAUSED = Symbol();
+
+class Player {
+    constructor(width = 64, height = 64) {
+        this.width = width;
+        this.height = height;
+        this.universe = Universe.new(width, height);
+        this.playing = false;
+
+        this.initialize_universe();
+    }
+
+    start() {
+        this.playing = true;
+    }
+
+    pause() {
+        this.playing = false;
+    }
+
+    toggleState() {
+        this.playing = !this.playing;
+    }
+
+    initialize_universe() {
+        for (var x = 0; x < this.width; x++) {
+            for (var y = 0; y < this.height; y++) {
+                const ix = x + y * this.height;
+                const alive = ix % 2 == 0 || ix % 7 == 0;
+                this.universe.set_cell(x, y, alive);
+            }
+        }
+    }
+
+    tick() {
+        if (this.playing) {
+            this.universe = this.universe.tick();
+            return true;
+        } else {
+            return false;
+        }
+    }
+
+    render(targetElement) {
+        targetElement.textContent = this.universe.render();
+    }
+}
+
+const player = new Player(128, 48);
+
+const renderLoop = () => {
+    const changed = player.tick();
+    if (changed) {
+        player.render(pre);
+        console.log("Rendered");
+    }
+
+    requestAnimationFrame(renderLoop);
+};
+
+requestAnimationFrame(renderLoop);
+
+document.onmousedown = _ => player.start();
+document.onmouseup = _ => player.pause();
+player.render(pre);
